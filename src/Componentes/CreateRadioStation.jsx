bdecfd2f78ba1f69ed7808c407f89ec6fdbc9e3e
jhonnydrummer <arantes.designergrafico@gmail.com>
2025-01-31 17:48:14 +0000

    ipv

--- src/Componentes/CreateRadioStation.jsx
+++ src/Componentes/CreateRadioStation.jsx
@@ -39,7 +39,7 @@ const CreateRadioStation = ({ onClose, onCreate }) => {
       
       const streamKey = uuidv4();
       const streamMount = `/radio_${currentUser.uid}.mp3`;
-      const serverUrl = "http://meuradio.com"; // Substituir pelo URL correto
+      const serverUrl = "http://[2001:818:e7ed:8e00:7c01:d6be:ef5:a6da]";
       const port = "8000"; // Substituir pela porta correta
       const fullStreamUrl = `${serverUrl}:${port}${streamMount}`;
       
